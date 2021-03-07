66c7a54708c81a93d66b5cb6f85e0a652d17fb96
Markus Pfeifer <markus.pfeifer@orquidea.de>
2021-03-07 22:35:04 +0100

    church encoding

--- Free.xctemplate/___FILEBASENAME___.swift
+++ Free.xctemplate/___FILEBASENAME___.swift
@@ -13,9 +13,9 @@ public struct ___VARIABLE_productName___<T> {
     fileprivate init(_ kind: Kind){self.kind = kind}
     
     public enum Kind {
-        case add(Int)
-        case remove(Int)
-        case filter((Int) -> Bool)
+        case add(Int, continuation: (Bool) -> T)
+        case remove(Int, continuation: (Bool) -> T)
+        case filter((Int) -> Bool, continuation: ([Int]) -> T)
     }
 
 }
@@ -24,11 +24,11 @@ public struct ___VARIABLE_productName___<T> {
 extension ___VARIABLE_productName___ where T == Bool {
     
     static func add(_ value: Int) -> Self {
-        ___VARIABLE_productName___(.add(value))
+        ___VARIABLE_productName___(.add(value, continuation: {$0}))
     }
     
     static func remove(_ value: Int) -> Self {
-        ___VARIABLE_productName___(.remove(value))
+        ___VARIABLE_productName___(.remove(value, continuation: {$0}))
     }
     
 }
@@ -37,7 +37,7 @@ extension ___VARIABLE_productName___ where T == Bool {
 extension ___VARIABLE_productName___ where T == [Int] {
     
     static func getAll(_ pred: @escaping (Int) -> Bool) -> Self {
-        ___VARIABLE_productName___(.filter(pred))
+        ___VARIABLE_productName___(.filter(pred, continuation: {$0}))
     }
     
 }
